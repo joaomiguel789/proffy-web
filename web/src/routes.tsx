350a1fc97473ab7b9e23c0a0441a9785f6999bfe
João Miguel <joao.martins789@gmail.com>
2020-08-13 12:44:01 -0300

    creating css screen login

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
+import TeacherLogin from './pages/TeacherLogin';
 
 function Routes() {
   return(
@@ -10,6 +11,7 @@ function Routes() {
       <Route path="/" exact component={Landing}/>
       <Route path="/study" component={TeacherList}/>
       <Route path="/give-classes" component={TeacherForm}/>
+      <Route path="/login" component={TeacherLogin} />
     </BrowserRouter>
   );
 }
